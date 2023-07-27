a4d98b6b136f9e5d641d25ca60718f7dd2a13160
Alex Luowan <aaluowan@gmail.com>
2023-07-26 19:07:29 -0700

    Updated version of site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily:{
+        brut: ["OT-Brut"],
+        montreal: ["PP-Neue-Montreal"],
+        eiko: ["PP-Eiko"],
+        untitledsans: ["Untitled-Sans"],
+      },
+      colors: {
+        customGray: '#7C7C7C',
+      },
+      fontSize: {
+        '5xl': '3rem', // Change the font size to your desired size
+      },
+      lineHeight: {
+        '5xl': '137%',  // Change the line height to your desired value
+      },
+      margin: {
+        'custom-top': '27rem', // Your custom margin value
+      },
+    },
   },
   plugins: [],
 }
