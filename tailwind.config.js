17b3b20628faf2786e671562bf96efe675a41316
Alex Luowan <aaluowan@gmail.com>
2023-08-04 12:20:23 -0700

    Made index.html responsive to mobile and tablet view

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,13 @@ module.exports = {
       },
       fontSize: {
         '5xl': '3rem', // Change the font size to your desired size
-        '4xl': '2.5rem'
+        '4xl': '2.5rem',
+        '3xl': '2rem'
       },
       lineHeight: {
         '5xl': '137%',  // Change the line height to your desired value
         '4xl': '137%',  // Change the line height to your desired value
+        '3xl': '137%',  // Change the line height to your desired value
       },
       margin: {
         'custom-top': '27rem', // Your custom margin value
